8c03ea0f42c6681ca6ba9a4878492c63a144f3f3
eric7733 <s1000am@163.com>
2023-02-18 22:09:13 +0800

    add ch3

--- chapter_03/bicycles.py
+++ chapter_03/bicycles.py
@@ -1,4 +1,5 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 message = f"My first bicycle was a {bicycles[0].title()}."
-
-print(message)
+print(bicycles)  # ['trek', 'cannondale', 'redline', 'specialized']
+print(message)  # My first bicycle was a Trek.
+print(bicycles[-1])  # specialized
--- chapter_03/motorcycles.py
+++ chapter_03/motorcycles.py
@@ -1,5 +1,11 @@
-motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
-print(motorcycles)
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+# print(motorcycles)    # ['honda', 'yamaha', 'suzuki', 'ducati']
+# motorcycles[0] = "理想L9"
+# print(motorcycles)    # ['理想L9', 'yamaha', 'suzuki', 'ducati']
+# motorcycles.append('BMW')
+# print(motorcycles) # ['honda', 'yamaha', 'suzuki', 'ducati', 'BMW']
+motorcycles.insert(1, "benz")
+print(motorcycles)  # ['honda', 'benz', 'yamaha', 'suzuki', 'ducati']
 
 too_expensive = 'ducati'
 motorcycles.remove(too_expensive)
